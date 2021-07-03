bd0aec721545f0241f916fcfe17f5f0b3e5989f8
ChicK00o <hirohitthegreat@gmail.com>
2021-07-03 09:57:14 +0530

    moving todo keyword faces to config file

--- config.el
+++ config.el
@@ -1,13 +1,22 @@
-(defvar gtd/org-agenda-files '("git/org"))
+;; (defvar gtd/org-agenda-files '("git/org"))
 
-(defvar gtd/org-default-notes-file "~/git/org/refile.org")
+;; (defvar gtd/org-default-notes-file "~/git/org/refile.org")
 
-(defvar gtd/org-agenda-diary-file "~/git/org/diary.org")
+;; (defvar gtd/org-agenda-diary-file "~/git/org/diary.org")
 
-
-(defvar bh/organization-task-id "e2fb68ed-2c63-4f32-9fa3-9ce17349191e")
+;; (defvar bh/organization-task-id "e2fb68ed-2c63-4f32-9fa3-9ce17349191e")
 
 (defvar bh/project-list nil)
 
 (defvar bh/hide-scheduled-and-waiting-next-tasks t)
 
+(defvar gtd/org-todo-keyword-faces
+  (quote (("TODO" :foreground "red" :weight bold)
+          ("NEXT" :foreground "blue" :weight bold)
+          ("DONE" :foreground "forest green" :weight bold)
+          ("WAITING" :foreground "orange" :weight bold)
+          ("HOLD" :foreground "magenta" :weight bold)
+          ("CANCELLED" :foreground "forest green" :weight bold)
+          ("MEETING" :foreground "forest green" :weight bold)
+          ("PHONE" :foreground "forest green" :weight bold)))
+  )
--- packages.el
+++ packages.el
@@ -251,15 +251,7 @@
 
   ;; ;; TODO Other todo keywords doesn't have appropriate faces yet. They should
   ;; ;; have faces similar to spacemacs defaults.
-  (setq org-todo-keyword-faces
-        (quote (("TODO" :foreground "red" :weight bold)
-                ("NEXT" :foreground "blue" :weight bold)
-                ("DONE" :foreground "forest green" :weight bold)
-                ("WAITING" :foreground "orange" :weight bold)
-                ("HOLD" :foreground "magenta" :weight bold)
-                ("CANCELLED" :foreground "forest green" :weight bold)
-                ("MEETING" :foreground "forest green" :weight bold)
-                ("PHONE" :foreground "forest green" :weight bold))))
+  (setq org-todo-keyword-faces gtd/org-todo-keyword-faces)
 
   (setq org-use-fast-todo-selection t)
 
